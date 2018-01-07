6eb2950b8eee64bc872f3a183bd2895e643fc3ff
Li Feng <fengli@smartx.com>
2018-01-07 22:38:10 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+# Get src rpm(including src and spec file) from http://vault.centos.org/centos/7/virt/Source/kvm-common/qemu-kvm-ev-2.9.0-16.el7_4.13.1.src.rpm
+# don't get from https://git.centos.org/r/rpms/qemu-kvm.git
 [ $1 -eq 1 ] &&  rpmbuild -bp  --short-circuit --target=$(uname -m)  --define "_sourcedir `pwd`" --define "_builddir `pwd`" qemu-kvm.spec
 [ $1 -eq 2 ] &&  rpmbuild -bc  --short-circuit --target=$(uname -m)   --define "_sourcedir `pwd`" --define "_builddir `pwd`" qemu-kvm.spec
 [ $1 -eq 3 ] &&  rpmbuild -bi  --short-circuit --target=$(uname -m)  --define "_sourcedir `pwd`" --define "_builddir `pwd`" qemu-kvm.spec
